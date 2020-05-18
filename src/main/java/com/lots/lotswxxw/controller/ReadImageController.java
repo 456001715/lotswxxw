c0bfd02ec1c22f21899aebae689c7e661ce36dd4
lots <553294090@qq.com>
2020-05-18 09:32:51 +0800

    1.添加頁面與登錄方法

--- src/main/java/com/lots/lotswxxw/controller/ReadImageController.java
+++ src/main/java/com/lots/lotswxxw/controller/ReadImageController.java
@@ -2,15 +2,14 @@ package com.lots.lotswxxw.controller;
 
 import com.lots.lotswxxw.domain.vo.JsouResult;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author: lots
  * @Date: 2020/1/15 09:47
  * @Description:
  */
-@Controller
+@RestController
 @RequestMapping("readImage")
 public class ReadImageController {
     @RequestMapping("index")
@@ -20,9 +19,9 @@ public class ReadImageController {
 
     }
 
-    @RequestMapping("gettext")
-    @ResponseBody
-    public JsouResult gettext(){
+    @PostMapping("gettext")
+    public JsouResult gettext(@RequestBody Object obj){
+        System.out.println(obj.toString());
         System.out.println("进入");
         return new JsouResult(200,"请求成功","123");
 
