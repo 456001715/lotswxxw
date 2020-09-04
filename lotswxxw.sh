b5e1ea69fa2e22da9ad6831def5ed47368ed7814
553294090@qq.com <553294090@qq.com>
2020-09-04 14:29:11 +0800

    添加启动脚本

--- lotswxxw.sh
+++ lotswxxw.sh
@@ -3,4 +3,5 @@ docker rm lotswxxw
 docker rmi lotswxxw
 cd /home/java/lotswxxw
 docker build -t lotswxxw .
+sleep 6s
 docker run --name lotswxxw -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone --restart=always -d -p 80:8080 lotswxxw
