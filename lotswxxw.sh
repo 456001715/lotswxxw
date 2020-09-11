9cb1d093a81c93a6bd7d6d0359ab793055af8551
553294090@qq.com <553294090@qq.com>
2020-09-11 14:33:43 +0800

    修改脚本

--- lotswxxw.sh
+++ lotswxxw.sh
@@ -1,7 +1,7 @@
-docker stop lotswxxw
-docker rm lotswxxw
-docker rmi lotswxxw
-cd /home/java/lotswxxw
-docker build -t lotswxxw .
-sleep 6s
-docker run --name lotswxxw -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone --restart=always -d -p 80:8080 lotswxxw
+docker stop lotswxxw;
+docker rm lotswxxw;
+docker rmi lotswxxw;
+cd /home/java/lotswxxw;
+docker build -t lotswxxw .;
+sleep 2s;
+docker run --name lotswxxw -v /etc/localtime:/etc/localtime -v /etc/timezone:/etc/timezone --restart=always -d -p 80:8080 lotswxxw;
