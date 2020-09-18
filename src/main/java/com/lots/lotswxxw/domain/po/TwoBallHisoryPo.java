5814e7fca90ff2a81447f25aae859c3cfe447666
553294090@qq.com <553294090@qq.com>
2020-09-18 14:16:59 +0800

    优化代码

--- src/main/java/com/lots/lotswxxw/domain/po/TwoBallHisoryPo.java
+++ src/main/java/com/lots/lotswxxw/domain/po/TwoBallHisoryPo.java
@@ -6,8 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-    import java.util.Date;
-    import com.fasterxml.jackson.annotation.JsonFormat;
+import java.util.Date;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 /**
 * 双色球开奖历史(two_ball_hisory)
@@ -22,24 +22,27 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class TwoBallHisoryPo implements java.io.Serializable {
 /** 版本号 */
-private static final long serialVersionUID = 1606833029074169593L;
+private static final long serialVersionUID = 2674870152760852750L;
 
-
-            /** 主键 */
-        @ApiModelProperty(value = "主键")
-        private Integer id;
-
-            /** 红球 */
-        @ApiModelProperty(value = "红球")
-        private String redNumber;
-
-            /** 篮球 */
-        @ApiModelProperty(value = "篮球")
-        private String blueNumber;
-
-            /** 创建时间 */
-        @ApiModelProperty(value = "创建时间")
-            @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
-        private Date creatTime;
-
+    /**
+	* 主键 
+	*/
+    @ApiModelProperty(value = "主键")
+    private Integer id;
+    /**
+	* 红球 
+	*/
+    @ApiModelProperty(value = "红球")
+    private String redNumber;
+    /**
+	* 篮球 
+	*/
+    @ApiModelProperty(value = "篮球")
+    private String blueNumber;
+    /**
+	* 创建时间 
+	*/
+    @ApiModelProperty(value = "创建时间")
+    @JsonFormat(timezone = "GMT+8", pattern = "yyyy-MM-dd HH:mm:ss")
+    private Date creatTime;
 }
