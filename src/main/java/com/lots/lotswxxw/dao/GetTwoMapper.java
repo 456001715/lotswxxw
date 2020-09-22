125e1ce2efe73081f5f6b43543fa7571376bc212
553294090@qq.com <553294090@qq.com>
2020-09-22 14:18:02 +0800

    优化注解

--- src/main/java/com/lots/lotswxxw/dao/GetTwoMapper.java
+++ src/main/java/com/lots/lotswxxw/dao/GetTwoMapper.java
@@ -10,14 +10,49 @@ import java.util.List;
  * @description:
  */
 public interface GetTwoMapper {
+
+    /**
+    * GetTwoMapper
+    * @author: lots
+    * @param entity:
+    * @return: java.util.List<com.lots.lotswxxw.domain.po.GetTwoPO>
+    * @date: 2020/9/22 11:26
+    */
     List<GetTwoPO> findGetTwoByCondition(GetTwoPO entity);
 
+    /**
+    * GetTwoMapper
+    * @author: lots
+    * @param entity: 
+    * @return: int
+    * @date: 2020/9/22 11:25
+    */
     int insertGetTwo(GetTwoPO entity);
 
+    /**
+    * GetTwoMapper
+    * @author: lots
+    * @param ids:
+    * @return: int
+    * @date: 2020/9/22 11:27
+    */
     int deleteGetTwoByIds(List<Integer> ids);
 
+    /**
+    * GetTwoMapper
+    * @author: lots
+    * @param entity:
+    * @return: int
+    * @date: 2020/9/22 11:27
+    */
     int updateGetTwo(GetTwoPO entity);
 
+    /**
+    * GetTwoMapper
+    * @author: lots
+    * @return: java.util.List<com.lots.lotswxxw.domain.po.GetTwoPO>
+    * @date: 2020/9/22 11:27
+    */
     List<GetTwoPO> getNowList();
 
 }
